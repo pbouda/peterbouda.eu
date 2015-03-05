2bcbd1c22ec8ccb2d4ac7652fadab21c76600f5c
Peter Bouda <pbouda@cidles.eu>
2015-03-05 16:51:03 +0000

    Updated publish script.

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
 pelican content -o output -s publishconf.py
-s3cmd sync output/ s3://www.peterbouda.eu --acl-public --delete-removed
+#s3cmd sync output/ s3://www.peterbouda.eu --acl-public --delete-removed
+s3cmd put -r output/ s3://www.peterbouda.eu --acl-public
