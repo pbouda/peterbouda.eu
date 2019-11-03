ed5db957a177d9152fd9ea34ca4c53e6a9d9ccee
Peter Bouda <hey@peterbouda.eu>
2019-11-03 13:53:20 +0100

    Update video links

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-pelican content -o /Users/pbouda/Nextcloud/Website -s publishconf.py
+pelican content -o /var/www/peterbouda.eu -s publishconf.py
 #s3cmd sync output/ s3://www.peterbouda.eu --acl-public --delete-removed
 #s3cmd put -r output/ s3://www.peterbouda.eu --acl-public
 #rsync -vrz -e ssh /home/pbouda/Projects/git-github/peterbouda.eu/output/* pbouda@h2407922.stratoserver.net:/var/www/peterbouda.eu/
