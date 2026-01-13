76cacf308433faa3507f4d944f1a7393b11fe2ec
Peter Bouda <pbouda@outlook.com>
2026-01-13 10:44:06 +0000

    feat: Update publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-pelican content -o output -s publishconf.py
-echo 'www.peterbouda.eu' > ./output/CNAME
-git pull
-ghp-import output -b gh-pages
-git push origin gh-pages
+pelican content -o /var/www/peterbouda.eu -s publishconf.py
